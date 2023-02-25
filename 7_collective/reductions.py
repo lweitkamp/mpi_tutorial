fb91f9401868b66489ad660d5df214e66bf29192
laurens <laurensweitkamp@thirona.eu>
2023-02-25 13:26:55 +0100

    add reduce and allreduce

--- 7_collective/reductions.py
+++ 7_collective/reductions.py
@@ -1,9 +1,13 @@
 from mpi4py import MPI
 
+
 def find_sum(vector):
     my_sum = 0.0
     for i in range(len(vector)):
         my_sum += vector[i]
+    
+    my_sum = MPI.COMM_WORLD.allreduce(my_sum, op=MPI.SUM)
+
     return my_sum
 
 def find_maximum(vector):
@@ -11,6 +15,9 @@ def find_maximum(vector):
     for i in range(len(vector)):
         if vector[i] > my_max:
             my_max = vector[i]
+    
+    my_max = MPI.COMM_WORLD.reduce(my_max, op=MPI.MAX, root=0)
+
     return my_max
 
 n_numbers = 1024
@@ -24,4 +31,5 @@ my_sum = find_sum(vector)
 print(f"The sum of the number is {my_sum}")
 
 my_max = find_maximum(vector)
-print(f"The largest number is {my_max}")
+if rank == 0:
+    print(f"The largest number is {my_max}")
